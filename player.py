aa10e53b4ea9f97dd74619a35cb47bd0a81a6d71
FranciscoFSilva <francisco.silva.e@gmail.com>
2026-08-17 11:03:14 +0100

    CH3:L2

---
+++ player.py
@@ -0,0 +1,21 @@
+from constants import PLAYER_RADIUS, LINE_WIDTH
+from circleshape import CircleShape
+import pygame
+
+class Player(CircleShape):
+    def __init__(self, x: float, y: float):
+        self.position = pygame.Vector2(x, y)
+        self.radius = PLAYER_RADIUS
+        self.rotation = 0
+
+    # in the Player class
+    def triangle(self) -> list[pygame.Vector2]:
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen: pygame.Surface) -> None:
+        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
